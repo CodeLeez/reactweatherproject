44ec3aae487542fa8b11abc45da72b2bda10e3d6
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2025-06-09 11:40:19 +0800

    imported bootstrap CSS in JS & completed basic HTML of app

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,30 @@
 import React from "react";
 import "./Weather.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
       <h1>Harare</h1>
       <ul>
         <li>Thursday 12:17</li>
         <li>Mostly Cloudy</li>
       </ul>
-
       <div className="row">
         <div className="col-6">
           <img
