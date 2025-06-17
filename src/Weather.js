8a331ca2c30ef23c2a2b758e32bb2e596fbb159c
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2025-06-17 14:47:32 +0800

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -12,10 +12,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -25,13 +30,18 @@ export default function Weather() {
         <li>Thursday 12:17</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />{" "}
-          6&deg;C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+
+            <span className="temperature">6</span>
+            <span className="unit">&deg;C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
