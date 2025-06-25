d0128e4025c3c8d78f06b1885db229344bac39fd
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2025-06-25 14:25:46 +0800

    Added Date  in the weather component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Harare" />
+        <Weather defaultCity="Paris" />
 
         <footer>
           This project was coded by{" "}
