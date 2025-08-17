e37c254856a23b0d52f90c8b351c362a1cb54c82
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2025-08-18 06:27:24 +0800

    WeatherForecast Layout

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -64,6 +65,7 @@ export default function Weather(props) {
         </form>
 
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
