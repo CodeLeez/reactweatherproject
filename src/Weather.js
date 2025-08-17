ed0a5049942392c74f142b2bcd764e0d671276db
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2025-08-18 07:33:00 +0800

    Weather Forecast API

--- src/Weather.js
+++ src/Weather.js
@@ -14,6 +14,7 @@ export default function Weather(props) {
     console.log(response.data);
     setWeatherData({
       ready: true,
+      coordinates: response.data.coordinates,
       temperature: response.data.temperature.current,
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
@@ -65,7 +66,7 @@ export default function Weather(props) {
         </form>
 
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
